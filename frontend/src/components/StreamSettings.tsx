f706051843c7c4ca1ea4dd9a79d69f8a739651fb
Jonas Nilsson <nilsson760@gmail.com>
2026-04-28 15:45:40 +0200

    finished implementing the streamsettings layout so it looks nice for all modes

--- frontend/src/components/StreamSettings.tsx
+++ frontend/src/components/StreamSettings.tsx
@@ -82,37 +82,107 @@ function getStreamPositionClass(streamCount: number, layout: ActiveLayout, index
     } 
     return positionClass
 }
+function getArrowPositionClass(direction: string, streamCount: number, activeLayout: ActiveLayout, currentIndex: number, targetIndex: number) {
+    if (streamCount === 3 && activeLayout === 'main-on-left' && currentIndex === 0) {
+        if (targetIndex === 1) {
+            return 'absolute right-1 top-1/4 -translate-y-1/2'
+        } if (targetIndex === 2) {
+            return 'absolute right-1 top-3/4 -translate-y-1/2'
+        }
+    } 
+    if (streamCount === 3 && activeLayout === 'main-on-top' && currentIndex === 0) {
+        if (targetIndex === 1) {
+            return 'absolute bottom-1 left-1/4 -translate-x-1/2'
+        } if (targetIndex === 2) {
+            return 'absolute bottom-1 left-3/4 -translate-x-1/2'
+        }
+    }
+    if (direction === 'up') {
+        return 'absolute top-1 left-1/2 -translate-x-1/2'
+    }
+    if (direction === 'down') {
+        return 'absolute bottom-1 left-1/2 -translate-x-1/2'
+    }
+    if (direction === 'left') {
+        return 'absolute left-1 top-1/2 -translate-y-1/2' 
+    }
+    if (direction === 'right') {
+        return 'absolute right-1 top-1/2 -translate-y-1/2'
+    }
+    return ''
+
+}
+function getArrowLabel(direction: string, streamCount: number, activeLayout: ActiveLayout, currentIndex: number, targetIndex: number) {
+    if (streamCount === 3 && activeLayout === 'main-on-left' && currentIndex === 0) {
+        if (targetIndex === 1) {
+            return '→'
+        }
+        if (targetIndex === 2) {
+            return '→'
+        }
+    }
+    if (streamCount === 3 && activeLayout === 'main-on-top' && currentIndex === 0) {
+        if (targetIndex === 1) {
+            return '↓'
+        }
+        if (targetIndex === 2) {
+            return '↓'
+        }
+    }
+    if (direction === 'up') {
+        return '↑'
+    }
+    if (direction === 'down') {
+        return '↓'
+    }
+    if (direction === 'left') {
+        return '←'
+    }
+    if (direction === 'right') {
+        return '→'
+    }
+    return ''
+}
+function getPreviewBoxClass(streamCount: number, layout: ActiveLayout, index: number) {
+    if (streamCount === 3 && layout === 'main-on-left' && index === 0) {
+        return 'h-68'
+    } if (streamCount === 3 && layout === 'main-on-top' && index === 0) {
+        return 'h-24'
+    }
+    return 'h-24'
+}
 
 export function StreamSettings({ streams, onRemoveStream, swapStream, activeLayout, }: StreamsettingsProps) {
-    const baseCardClass = 'relative rounded-xl border border-white/10 bg-zinc-800 px-3 py-2'
+    const baseCardClass = 'relative rounded-xl border border-white/10 bg-zinc-800 px-3 py-3'
     const containerClass = getSettingsGridClass(streams.length, activeLayout);
+    const baseArrowClass = 'rounded-lg px-2 py-1 text-xs font-medium text-blue-300 transition hover:bg-blue-500/10 hover:text-blue-200'
     return (
         <div className="w-120 absolute right-1 bottom-15 rounded-2xl border border-white/10 bg-zinc-900/95 p-3 text-zinc-100 shadow-lg backdrop-blur">
-            <p className="mb-3 text-sm font-semibold">Stream Settings</p>
+            <p className="mb-3 text-sm font-semibold pb-2">Stream Settings</p>
             <div className="space-y-2">
                 {streams.length === 0 && (
-                    <p className="text-sm text-zinc-400">Add a stream to manage order and removal.</p>
+                    <p className="text-sm text-zinc-400 ">Add a stream to manage order and removal.</p>
                 )}
                 {streams.length === 1 && (
-                    <div>
+                    <div className="relative">
                         {streams.map((stream, index) => (
                             <div
                                 key={stream.id}
-                                className="flex items-center rounded-xl border border-white/10 bg-zinc-800 px-3 py-2"
+                                className={baseCardClass}
                             >
                                 
-                            <div className="min-w-0">
-                                <span className="text-xs truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span>
-                                <div className="mb-2 grid h-14  ">
+                            <div className="relative min-w-0 w-full py-4 px-4">
+                                <span className="text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span>
+                                <div className="mb-2 grid h-25">
                                     <div className="rounded bg-zinc-600" />
                                 </div>
                             </div>
                             <button
                                 type="button"
                                 onClick={() => onRemoveStream(stream.id)}
-                                className="rounded-lg px-2 py-1 text-xs font-medium text-red-300 transition hover:bg-red-500/10 hover:text-red-200"
+                                className="rounded-lg px-2 py-1 text-xs font-medium text-red-300 transition hover:bg-red-500/10 hover:text-red-200 absolute top-7 right-6"
                             >
-                                Remove
+                                X
                             </button>
                             </div>
                         ))}
@@ -129,10 +199,16 @@ export function StreamSettings({ streams, onRemoveStream, swapStream, activeLayo
                                     key={stream.id}
                                     className={`${baseCardClass} ${positionClass} `}
                                 >
-                                    <div className="min-w-0">
-                                        <span className="text-xs truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span>
-                                        <div className="mb-2 grid h-24 gap-1">
-                                            {/* <span className="text-xs absolute truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span> */}
+                                    <div className="relative min-w-0 w-full py-4 px-4">
+                                        <div className={`relative mb-2 grid ${getPreviewBoxClass(streams.length, activeLayout, index)} gap-1 pl-1 pr-1`}>
+                                            <span className="text-xs absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span>
+                                             <button
+                                                type="button"
+                                                onClick={() => onRemoveStream(stream.id)}
+                                                className="rounded-lg px-2 py-1 text-xs font-medium text-red-300 transition hover:bg-red-500/10 hover:text-red-200 absolute top-0.5 right-1"
+                                            >
+                                                X
+                                            </button>
                                         <div className="rounded bg-zinc-600" />
                                     </div>
                                     </div>
@@ -140,95 +216,40 @@ export function StreamSettings({ streams, onRemoveStream, swapStream, activeLayo
                                         <button
                                             type="button"
                                             onClick={() => swapStream(index, moveTargets.up!)}
-                                            className="rounded-lg px-2 py-1 text-xs font-medium text-blue-300 transition hover:bg-blue-500/10 hover:text-blue-200 absolute top-1 left-1/2 -translate-x-1/2"
+                                            className={`${baseArrowClass} ${getArrowPositionClass('up', streams.length, activeLayout, index, moveTargets.up!)} `}
                                             >
-                                                ↑
+                                                {getArrowLabel('up', streams.length, activeLayout, index, moveTargets.up!)}
                                             </button>)}
                                     {moveTargets.down !== undefined && (
                                         <button
                                             type="button"
                                             onClick={() => swapStream(index, moveTargets.down!)}
-                                            className="rounded-lg px-2 py-1 text-xs font-medium text-blue-300 transition hover:bg-blue-500/10 hover:text-blue-200 absolute bottom-1 left-1/2 -translate-x-1/2 "
+                                            className={`${baseArrowClass} ${getArrowPositionClass('down', streams.length, activeLayout, index, moveTargets.down!)} `}
                                             >
-                                                ↓
+                                                {getArrowLabel('down', streams.length, activeLayout, index, moveTargets.down!)}
                                             </button>)}
                                     {moveTargets.left !== undefined && (
                                         <button
                                             type="button"
                                             onClick={() => swapStream(index, moveTargets.left!)}
-                                            className="rounded-lg px-2 py-1 text-xs font-medium text-blue-300 transition hover:bg-blue-500/10 hover:text-blue-200 absolute left-1 top-1/2 -translate-y-1/2"
+                                            className={`${baseArrowClass} ${getArrowPositionClass('left', streams.length, activeLayout, index, moveTargets.left!)}`}
                                             >
-                                                ←
+                                                {getArrowLabel('left', streams.length, activeLayout, index, moveTargets.left!)}
                                             </button>)}
                                     {moveTargets.right !== undefined && (
                                         <button
                                             type="button"
                                             onClick={() => swapStream(index, moveTargets.right!)}
-                                            className="rounded-lg px-2 py-1 text-xs font-medium text-blue-300 transition hover:bg-blue-500/10 hover:text-blue-200 absolute right-1 top-1/2 -translate-y-1/2"
+                                            className={`${baseArrowClass} ${getArrowPositionClass('right', streams.length, activeLayout, index, moveTargets.right!)}`}
                                             >
-                                                →
+                                                {getArrowLabel('right', streams.length, activeLayout, index, moveTargets.right!)}
                                             </button>)}
-                                    <button
-                                        type="button"
-                                        onClick={() => onRemoveStream(stream.id)}
-                                        className="rounded-lg px-2 py-1 text-xs font-medium text-red-300 transition hover:bg-red-500/10 hover:text-red-200"
-                                    >
-                                        Remove
-                                    </button>
+                                   
                                 </div>
                                 )
                             })}
                         </div>
                     )}
-
-                {/* {streams.length === 3 && (
-                    <div>
-                        {streams.map((stream, index) => {
-                            return (
-                            <div
-                                key={stream.id}
-                                className="flex items-center justify-between rounded-xl border border-white/10 bg-zinc-800 px-3 py-2"
-                            >
-                                <div className="min-w-0">
-                                   <span className="text-xs truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span>
-                                    <p className="text-xs text-zinc-400">Reordering goes here next</p>
-                                </div>
-                                <button
-                                    type="button"
-                                    onClick={() => onRemoveStream(stream.id)}
-                                    className="rounded-lg px-2 py-1 text-xs font-medium text-red-300 transition hover:bg-red-500/10 hover:text-red-200"
-                                >
-                                    Remove
-                                </button>
-                            </div>
-                            )
-                        })}
-                    </div>
-                )}
-                {streams.length === 4 && (
-                    <div>
-                        {streams.map((stream, index) => {
-                            return (
-                                <div
-                                    key={stream.id}
-                                    className="flex items-center justify-between rounded-xl border border-white/10 bg-zinc-800 px-3 py-2"
-                                >
-                                    <div className="min-w-0">
-                                    <span className="text-xs truncate font-medium text-zinc-300">{index + 1}. {stream.title}</span>
-                                        <p className="text-xs text-zinc-400">Reordering goes here next</p>
-                                    </div>
-                                    <button
-                                        type="button"
-                                        onClick={() => onRemoveStream(stream.id)}
-                                        className="rounded-lg px-2 py-1 text-xs font-medium text-red-300 transition hover:bg-red-500/10 hover:text-red-200"
-                                    >
-                                        Remove
-                                    </button>
-                                </div>
-                            )
-                        })}
-                    </div>
-                )}  */}
             </div>
         </div>
     )
